17c612a4508dafdc73354c999cd0d807436ee5ca
gzy <1522780701@qq.com>
2018-03-23 17:42:26 +0800

    demo0323

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,69 @@ const app = getApp()
 
 Page({
   data: {
+    item: {
+      index: 0,
+      msg: 'this is a template',
+      time: '2016-09-15'
+    },
+    length:10,
+    objectArray: [
+      { id: 5, unique: 'unique_5' },
+      { id: 4, unique: 'unique_4' },
+      { id: 3, unique: 'unique_3' },
+      { id: 2, unique: 'unique_2' },
+      { id: 1, unique: 'unique_1' },
+      { id: 0, unique: 'unique_0' },
+    ],
+    numberArray: [1, 2, 3, 4],
     motto: 'Hello World',
+    zero: 0,
+    id:0,  
+    a: 1,
+    b: 2,
+    c: 3,
+    condition:true,
     userInfo: {},
     hasUserInfo: false,
     msg: '',
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+
+  //事件
+  tapName: function (event) {
+    console.log(event)
+  },
+
+  //wx:key 示例事件
+  switch: function (e) {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function (e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{ id: length, unique: 'unique_' + length }].concat(this.data.objectArray)
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addNumberToFront: function (e) {
+    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
+  },
+
+
+
   //获取实例
   getAppInst:function(){
     console.log(getApp().globalData)
@@ -18,7 +75,8 @@ Page({
   clickMe: function () {
     //console.log(this.msg)
     //temp = this.msg
-    this.setData({ motto: this.data.motto + app.globalData.userInfo.nickName})
+    //this.setData({ motto: this.data.motto + app.globalData.userInfo.nickName})
+    //this.setData({ motto: this.data.motto + "12" })
     //this.setData({ msg: app.globalData.userInfo.nickName+"Hello World" })
   },
   //点击地图
